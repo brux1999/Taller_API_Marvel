02e8c3b304e445c47242e1f16b84cfe8345f9403
brux1999 <bvidal2018@gmail.com>
2023-09-08 17:06:50 -0300

    v1.3

--- js/index.js
+++ js/index.js
@@ -1,43 +1,18 @@
-// function showCharacters() {
-//         const apiUrl = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=8b952c8b38e25f1426591a1dbb023326&hash=72134039e0d32f9b5eb290473134bb6b';
-//         const container = document.querySelector('.container');
-//         let contentHTML = '';
-//         const loadMoreBtn = document.getElementById('loadMore');
+// Ejecuta funciones al cargar la página
+document.addEventListener ('DOMContentLoaded', ()=>{
+    showCharacters();
+});
 
-//         // Solicitud al API y construcción de las tarjetas en el HTML
-
-//         fetch(apiUrl)
-//             .then(res => res.json())
-//             .then((json) => {
-//                 for (const hero of json.data.results) {
-//                     let urlWiki = hero.urls[0].url;
-//                     contentHTML += `
-//                     <div id="marvel-cards">
-//                         <h2 class="heroName">${hero.name}</h2>
-//                         <p class='cardInfo'>click en la imagen para ver <br> comics donde aparece el personaje</p>
-//                         <a href="${urlWiki}" target="_blank">
-//                             <img class="heroImg" src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}">
-//                         </a>
-//                     </div>`;
-//                 }
-//                 container.innerHTML = contentHTML;
-//             })
-//             .catch(error => console.error('Error:', error));
-//     }
-    
-//     showCharacters();
-
+const apiKey = '8b952c8b38e25f1426591a1dbb023326';
+const container = document.querySelector('.container > .row');
+const loadMoreBtn = document.getElementById('loadMore');
+const searchInput = document.getElementById('searchInput')
 let offset = 0; // Variable para realizar paginación
 
-function showCharacters() {
-    const apiKey = '8b952c8b38e25f1426591a1dbb023326';
-    const container = document.querySelector('.container');
-    const loadMoreBtn = document.getElementById('loadMore');
-
     // Función para hacer la solicitud al API y construir las tarjetas en el HTML
-    function fetchCharacters() {
-        const apiUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${apiKey}&hash=72134039e0d32f9b5eb290473134bb6b&limit=10&offset=${offset}`;
-        
+    function showCharacters() {
+        const apiUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${apiKey}&hash=72134039e0d32f9b5eb290473134bb6b&limit=12&offset=${offset}`;
+
         fetch(apiUrl)
             .then(res => res.json())
             .then((json) => {
@@ -45,25 +20,30 @@ function showCharacters() {
                 for (const hero of json.data.results) {
                     let urlWiki = hero.urls[0].url;
                     contentHTML += `
-                    <div id="marvel-cards">
-                        <h2 class="heroName">${hero.name}</h2>
-                        <p class='cardInfo'> ${hero.description ? hero.description : 'No hay descripción disponible'}</p>
-                        <a href="${urlWiki}" target="_blank">
-                            <img class="heroImg" src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}">
-                        </a>
+                    <div class="col-12 col-md-4 col-lg-3 d-flex mt-5">
+                        <div class="card text-center text-bg-dark mb-3 border-danger mb-3" id="marvel-cards">
+                            <div class="card-header">
+                                <h2 class="card-title">${hero.name}</h2>
+                            </div>
+                                <a href="${urlWiki}" target="_blank">
+                                 <img class="card-img-top" src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}">
+                                </a>
+                            <div class="card-body">
+                                <p class='card-text'> ${hero.description ? hero.description : 'No hay descripción disponible'}</p>
+                            </div>
+                            <div class="card-footer text-danger">
+                                Click the image to see related comics
+                            </div> 
+                        </div>      
                     </div>`;
                 }
-                container.innerHTML += contentHTML;
-                offset += 10; // Incrementar el offset para la próxima solicitud
+  
+                container.innerHTML += contentHTML; // Agrega el contenido al HTML
+                offset += 12; // Incrementar el offset para la próxima solicitud
+                allCharacters = allCharacters.concat(json.data.results);
             })
-            .catch(error => console.error('Error:', error));
-    }
-
+            .catch(error => console.error('Error:', error)) 
+    } 
+    
     // Evento para cargar más contenido al hacer clic en el botón "Load More"
-    loadMoreBtn.addEventListener('click', fetchCharacters);
-
-    // Mostrar los primeros personajes al cargar la página
-    fetchCharacters();
-}
-
-showCharacters();
+    loadMoreBtn.addEventListener('click', showCharacters);
